8dd85193b279be4b03f287c04848ea188dc5a2dd
jack-hammr <97753993+jack-hammr@users.noreply.github.com>
2022-01-16 19:46:45 -0500

    final commit

---
+++ Jabaris_Adventure.py
@@ -0,0 +1,386 @@
+
+import os
+
+
+
+start = input("""
+        _       _                _ _                   _                 _                  
+      | |     | |              (_| )         /\      | |               | |                 
+      | | __ _| |__   __ _ _ __ _|/ ___     /  \   __| |_   _____ _ __ | |_ _   _ _ __ ___ 
+  _   | |/ _` | '_ \ / _` | '__| | / __|   / /\ \ / _` \ \ / / _ \ '_ \| __| | | | '__/ _ 
+ | |__| | (_| | |_) | (_| | |  | | \__ \  / ____ \ (_| |\ V /  __/ | | | |_| |_| | | |  __/
+  \____/ \__,_|_.__/ \__,_|_|_ |_| |___/ /_/    \_\__,_| \_/_\___|_| |_|\__|\__,_|_|  \___|
+ | |                        | |           | |   | |        | |              (_)            
+ | |_ _   _ _ __   ___   ___| |_ __ _ _ __| |_  | |_ ___   | |__   ___  __ _ _ _ __        
+ | __| | | | '_ \ / _ \ / __| __/ _` | '__| __| | __/ _ \  | '_ \ / _ \/ _` | | '_ \       
+ | |_| |_| | |_) |  __/ \__ \ || (_| | |  | |_  | || (_) | | |_) |  __/ (_| | | | | |      
+  \__|\__, | .__/ \___| |___/\__\__,_|_|   \__|  \__\___/  |_.__/ \___|\__, |_|_| |_|      
+       __/ | |                                                          __/ |              
+      |___/|_|                                                         |___/                                                
+""")
+
+
+while start != "start":
+    os.system("clear")
+    start = input("""
+        _       _                _ _                   _                 _                  
+      | |     | |              (_| )         /\      | |               | |                 
+      | | __ _| |__   __ _ _ __ _|/ ___     /  \   __| |_   _____ _ __ | |_ _   _ _ __ ___ 
+  _   | |/ _` | '_ \ / _` | '__| | / __|   / /\ \ / _` \ \ / / _ \ '_ \| __| | | | '__/ _ 
+ | |__| | (_| | |_) | (_| | |  | | \__ \  / ____ \ (_| |\ V /  __/ | | | |_| |_| | | |  __/
+  \____/ \__,_|_.__/ \__,_|_|_ |_| |___/ /_/    \_\__,_| \_/_\___|_| |_|\__|\__,_|_|  \___|
+ | |                        | |           | |   | |        | |              (_)            
+ | |_ _   _ _ __   ___   ___| |_ __ _ _ __| |_  | |_ ___   | |__   ___  __ _ _ _ __        
+ | __| | | | '_ \ / _ \ / __| __/ _` | '__| __| | __/ _ \  | '_ \ / _ \/ _` | | '_ \       
+ | |_| |_| | |_) |  __/ \__ \ || (_| | |  | |_  | || (_) | | |_) |  __/ (_| | | | | |      
+  \__|\__, | .__/ \___| |___/\__\__,_|_|   \__|  \__\___/  |_.__/ \___|\__, |_|_| |_|      
+       __/ | |                                                          __/ |              
+      |___/|_|                                                         |___/                                                
+""")
+
+print ("are you excited to go to the pool? says dad. yes! you reply, you've been wanting to go to the pool for so long, and now you get to go, and even have fun with your friends!")
+
+
+entrance = ("""
+as you arrive at the pool, you look around and see a bunch of your friends! go have fun, says your dad.
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                  3                |
+|    2                          4   |
+|             ~~~~~~~~~~~~          |
+‾             ~~~~~~~~~~~~          |
+_    1        ~~~~~~~~~~~~      5   |
+|             ~~~~~~~~~~~~          |
+|                                   |
+|    8             7            6   |
+|___________________________________|
+
+you are at the entrance!, type the number you want to go to to move! once you've moved around a bit, try the command "look around" to find some cool stuff!
+""")
+
+print (entrance)
+
+area1 = ("""
+you are at the entrance, type the number you want to go to to move! once you've moved around a bit, try the command look around to find some cool stuff!
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                  3                |
+|    2                          4   | 
+|             ~~~~~~~~~~~~          |
+‾             ~~~~~~~~~~~~          |
+_   you       ~~~~~~~~~~~~      5   |
+|             ~~~~~~~~~~~~          |
+|                                   |
+|    8             7            6   |
+|___________________________________|
+
+""")
+
+area2 = ("""
+as you reach your destination, you can see a door that seems to lead to the bathroom, a creepy guy in a cloak says, come back when you have the swimsuit, to you
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                  3                |
+|   you                         4   |
+|             ~~~~~~~~~~~~          |
+‾             ~~~~~~~~~~~~          |
+_    1        ~~~~~~~~~~~~      5   |
+|             ~~~~~~~~~~~~          |
+|                                   |
+|    8             7            6   |
+|___________________________________|
+(if you have the swimsuit, try "give creepy guy swimsuit" to give it to him)
+""")
+
+area3 = ("""
+you see a kid that doesn't seem to have a towel, but is really wet! he says: "please get me a towel! i'll do anything! even distract a lifeguard!"
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                 you               |
+|    2                          4   |
+|             ~~~~~~~~~~~~          |
+‾             ~~~~~~~~~~~~          |
+_    1        ~~~~~~~~~~~~      5   |
+|             ~~~~~~~~~~~~          |
+|                                   |
+|    8             7            6   |
+|___________________________________|
+(if you have a towel type "give towel" to help the kid!)
+""")
+
+area4 = ("""
+as you start nearing the backside of the pool, you see more adults, including your dad who is currently lounging in a chair. “hi! “ says your dad “can you get me some sunscreen? Thanks so much!”
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                  3                |
+|    2                         you  |
+|             ~~~~~~~~~~~~          |
+‾             ~~~~~~~~~~~~          |
+_    1        ~~~~~~~~~~~~      5   |
+|             ~~~~~~~~~~~~          |
+|                                   |
+|    8             7            6   |
+|___________________________________|
+""")
+
+area5 = ("""
+you see an eerie door thats guarded by a lifeguard, and seems to require a key to open
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                  3                |
+|    2                          4   |
+|             ~~~~~~~~~~~~          |
+‾             ~~~~~~~~~~~~          |
+_    1        ~~~~~~~~~~~~     you  |
+|             ~~~~~~~~~~~~          |
+|                                   |
+|    8             7            6   |
+|___________________________________|
+ type "sneak into diving pool" if you have a distraction and a key!
+""")
+
+area6 = ("""
+all you see are a couple of adults lounging and a couple of bushes
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                  3                |
+|    2                          4   |
+|             ~~~~~~~~~~~~          |
+‾             ~~~~~~~~~~~~          |
+_    1        ~~~~~~~~~~~~      5   |
+|             ~~~~~~~~~~~~          |
+|                                   |
+|    8             7           you  |
+|___________________________________|
+""")
+
+area7 = ("""
+all you see are a couple of adults lounging and a couple of bushes
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                  3                |
+|    2                          4   |
+|             ~~~~~~~~~~~~          |
+‾             ~~~~~~~~~~~~          |
+_    1        ~~~~~~~~~~~~      5   |
+|             ~~~~~~~~~~~~          |
+|                                   |
+|    8            you           6   |
+|___________________________________|
+""")
+
+area8 = ("""as you reach your destination, you see a small shop that seems to have a bunch of supplies available for purchase. You can buy the following items
+Sunscreen: 5$
+swimsuits 10$
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                  3                |
+|    2                          4   |
+|             ~~~~~~~~~~~~          |
+‾             ~~~~~~~~~~~~          |
+_    1        ~~~~~~~~~~~~      5   |
+|             ~~~~~~~~~~~~          |
+|                                   |
+|   you            7            6   |
+|___________________________________|
+""")
+
+
+
+money = 0
+sunscreen = False
+swimsuit = False
+key = False
+distraction = False
+win = False
+towel = False
+area1cmd = 0
+area2cmd = 0
+area3cmd = 0
+area4cmd = 0
+area5cmd = 0
+area6cmd = 0
+area7cmd = 0
+area8cmd = 0
+
+
+
+while win == False:
+    area1cmd = 0
+    area2cmd = 0
+    area3cmd = 0
+    area4cmd = 0
+    area5cmd = 0
+    area6cmd = 0
+    area7cmd = 0
+    area8cmd = 0
+    location2 = input("where would you like to go?: ")
+    if location2 == "1":
+        os.system("clear")
+        print(area1) 
+        area1cmd = input ("enter a command: ")
+    elif location2 == "2":
+        os.system("clear")
+        print (area2)
+        area2cmd = input ("enter a command: ")
+    elif location2 == "3":
+        os.system("clear")
+        print (area3)
+        area3cmd = input ("enter a command: ")
+    elif location2 == "4":
+        os.system("clear")
+        print (area4)
+        area4cmd = input ("enter a command: ")
+    elif location2 == "5":
+        os.system("clear")
+        print (area5)
+        area5cmd = input ("enter a command: ")
+    elif location2 == "6":
+        os.system("clear")
+        print (area6)
+        area6cmd = input ("enter a command: ")
+    elif location2 == "7":
+        os.system("clear")
+        print (area7)
+        area7cmd = input ("enter a command: ")
+    elif location2 == "8":
+        os.system("clear")
+        print (area8) 
+        area8cmd = input ("enter a command: ")
+    else:
+        print ("that is not a valid location: ")
+
+    if area1cmd  == "look around":
+        print ("you found 3 dollars!")
+        money = money + 3
+        area1cmd == 0
+        print ("you now have this much money!")
+        print (money)   
+        
+    if area2cmd  == "look around":
+        print ("you found 3 dollars!")
+        money = money + 3
+        area2cmd == 0
+        print ("you now have this much money!")
+        print (money)
+   
+    if area3cmd  == "look around":
+        print ("you found 3 dollars!")
+        money = money + 3
+        area3cmd == 0
+        print ("you now have this much money!")
+        print (money)
+        
+    if area4cmd  == "look around":
+        print ("you found 3 dollars!")
+        money = money + 3
+        area4cmd == 0
+        print ("you now have this much money!")
+        print (money)
+    
+    if area5cmd  == "look around":
+        print ("you found 3 dollars!")
+        money = money + 3
+        area5cmd == 0
+        print ("you now have this much money!")
+        print (money)
+        
+    if area6cmd  == "look around":
+        print ("you found 3 dollars!")
+        money = money + 3
+        area6cmd == 0
+        print ("you now have this much money!")
+        print (money)
+    
+        print ("")
+    if area7cmd  == "look around":
+        print ("you found 3 dollars!")
+        money = money + 3
+        area7cmd == 0
+        print ("you now have this much money!")
+        print (money)
+  
+    if area8cmd  == "look around":
+        print ("you found 3 dollars!")
+        money = money + 3
+        area8cmd == 0
+        print ("you now have this much money!")
+        print (money)
+   
+
+    if area5cmd  == "sneak into diving pool":
+        if distraction == True:
+ 
+                os.system("clear")       
+                divingpool1 = input("""  
+    are you ready to go diving jabari?      
+    |‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+    |                                   |
+    |                                   |
+    |             ~~~~~~~~~~~~          |
+    ‾             ~~~~~~~~~~~~          |
+    _             ~~~~~~~~☐☐☐☐H   0     |
+    |             ~~~~~~~~~~~~H  /|\    |
+    |                             A     |
+    |                                   |
+    |___________________________________|       
+            """)
+
+                if divingpool1 == "yes":
+                    print ("""     
+    |‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+    |                                   |
+    |                                   |
+    |             ~~~*~~~~*~~~          |
+    ‾             ~~~~~**~~~~~          |
+    _             ~~~~~**~☐☐☐☐H         |
+    |             ~~~*~~~~*~~~H         |
+    |                                   |
+    |                                   |
+    |___________________________________|    
+    good job jabari! you finally worked up the courage to go diving! (even if it took a little deception and bribery)   
+            """)
+                    win == True
+
+
+
+    if area8cmd == "buy sunscreen":
+        if money >= 5:
+            sunscreen = True
+            print ("you now have sunscreen!")
+            money = money - 5
+        else: 
+            print ("you don't have enough money")
+
+    if area8cmd == "buy swimsuit":
+        if money >= 10:
+            swimsuit = True
+            print ("you now have a swimsuit!")
+            money = money - 10
+        else:
+            print ("you don't have enough money")
+
+
+    
+    if area4cmd == "give dad sunscreen":  
+        print ("thanks Jabari! here, have this towel for when you go into the pool later!")
+        towel == "true"
+    
+    if area3cmd == "give towel":
+        print ("thanks!, the kid says, i guess i'll go distract that lifeguard, if you really want me to")
+        distraction == True
+
+    if area2cmd == "give creepy guy swimsuit":
+        print("""thank you very much *cackles* here have this as a token of my appreciation
+        the creepy guy gave you a key!
+        """)
+        key = True
+
+    if distraction and key == True:
+        area5 = (""" you see an eerie door thats guarded by a lifeguard, and seems to require a key to open
+    |‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+    |                  3                |
+    |    2                          4   |
+    |             ~~~~~~~~~~~~          |
+    ‾             ~~~~~~~~~~~~          |
+    _    1        ~~~~~~~~~~~~     you  |
+    |             ~~~~~~~~~~~~          |
+    |                                   |
+    |    8             7            6   |
+    |___________________________________|
+    type "sneak into diving pool" if you have a distraction and a key!
+    """)
+    
+    
+    
